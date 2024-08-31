febbffb2f881f287bceb783dc127c0f121e63278
Marta Smuk <msmuk999@gmail.com>
2024-08-31 11:06:56 +0200

    added File with Albums for commmon use

--- src/models/Album.tsx
+++ src/models/Album.tsx
@@ -1,11 +1,9 @@
-// Define the Album interface
 export interface Album {
   userId: number;
   id: number;
   title: string;
 }
 
-// Define the context interface
 export interface AlbumsContextInterface {
   albums: Album[];
   setAlbums: React.Dispatch<React.SetStateAction<Album[]>>;
